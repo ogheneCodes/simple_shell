b0ae606837081b9a84cb6617dfc96256c31ef2f3
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2022-09-09 04:26:08 -0700

    shell

--- shell_loop.c
+++ shell_loop.c
@@ -1,36 +1,39 @@
-# comments from the input
+#include "main.h"
+
+/**
+ * without_comment - deletes comments from the input
  *
  * @in: input string
  * Return: input without comments
  */
- char *without_comment(char *in)
- {
-   int i, up_to;
+char *without_comment(char *in)
+{
+  int i, up_to;
 
-   up_to = 0;
-   for (i = 0; in[i]; i++)
-     {
-       if (in[i] == '#')
-	 {
-	   if (i == 0)
-	     {
-	       free(in);
-	       return (NULL);
-	     }
+  up_to = 0;
+  for (i = 0; in[i]; i++)
+    {
+      if (in[i] == '#')
+	{
+	  if (i == 0)
+	    {
+	      free(in);
+	      return (NULL);
+	    }
 
-	   if (in[i - 1] == ' ' || in[i - 1] == '\t' || in[i - 1] == ';')
-	     up_to = i;
-	 }
-     }
+	  if (in[i - 1] == ' ' || in[i - 1] == '\t' || in[i - 1] == ';')
+	    up_to = i;
+	}
+    }
 
-   if (up_to != 0)
-     {
-       in = _realloc(in, i, up_to + 1);
-       in[up_to] = '\0';
-     }
+  if (up_to != 0)
+    {
+      in = _realloc(in, i, up_to + 1);
+      in[up_to] = '\0';
+    }
 
-   return (in);
- }
+  return (in);
+}
 
 /**
  * shell_loop - Loop of shell
